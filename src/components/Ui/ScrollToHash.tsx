9fc2bdfaf0c3092830be58f5ea11d435286530eb
fateme <fatemekhs20@gmail.com>
2026-09-02 12:29:15 +0330

    fix: improve page scroll behavior and dev server

--- src/components/Ui/ScrollToHash.tsx
+++ src/components/Ui/ScrollToHash.tsx
@@ -2,20 +2,26 @@ import { useEffect } from "react";
 import { useLocation } from "react-router";
 
 const ScrollToHash = () => {
-  const { hash } = useLocation();
+  const { hash, pathname } = useLocation();
 
   useEffect(() => {
-  if (!hash) return;
+    if (hash) {
+      const id = hash.replace("#", "");
 
-  const id = hash.replace("#", "");
+      const timer = setTimeout(() => {
+        const element = document.getElementById(id);
 
-  const timer = setTimeout(() => {
-    const element = document.getElementById(id);
-    element?.scrollIntoView({ behavior: "smooth", block: "start" });
-  }, 100);
+        element?.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }, 100);
 
-  return () => clearTimeout(timer);
-}, [hash]);
+      return () => clearTimeout(timer);
+    }
+
+    window.scrollTo(0, 0);
+  }, [pathname, hash]);
 
   return null;
 };
